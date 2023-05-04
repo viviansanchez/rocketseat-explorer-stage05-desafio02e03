29353cba513ae77217bc23ebcd30de2afdda27a0
Vivian Sanchez Desktop <visasal88@gmail.com>
2023-05-04 14:01:57 -0300

    refactor: html and css - better semantics

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const teste = document.querySelector('#teste')
+
+// teste.addEventListener('click', playAudio)
+
+// function playAudio() {
+//   const forestAudio = new Audio('../assets/audios/forest.wav')
+//   forestAudio.play()
+// }
