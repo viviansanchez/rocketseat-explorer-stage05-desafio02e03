0794fed95a9d2e13bb104a5658e3d2f1f74f885f
Vivian Sanchez Laptop <visasal88@gmail.com>
2023-05-07 23:24:05 -0300

    fix: now card sounds actually start playing at 50% volume

--- assets/js/sounds.js
+++ assets/js/sounds.js
@@ -1,3 +1,5 @@
+import { forestVolume, rainVolume,coffeeShopVolume, fireplaceVolume } from "./elements.js"
+
 export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
   const forestAudio = new Audio('./assets/audios/forest.wav')
   const rainAudio = new Audio('./assets/audios/rain.wav')
@@ -6,6 +8,12 @@ export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
   const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
 
   function playForestAudio() {
+    const checkActiveSound = btnForest.classList.contains('active-sound')
+    if(!checkActiveSound) {
+      forestVolume.value = 50
+      forestAudio.volume = forestVolume.value / 100
+    }
+
     stopAudioPlaying()
     forestAudio.play()
     forestAudio.loop = true
@@ -17,6 +25,12 @@ export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
   }
   
   function playRainAudio() {
+    const checkActiveSound = btnRain.classList.contains('active-sound')
+    if(!checkActiveSound) {
+      rainVolume.value = 50
+      rainAudio.volume = rainVolume.value / 100
+    }
+
     stopAudioPlaying()
     rainAudio.play()
     rainAudio.loop = true
@@ -28,6 +42,12 @@ export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
   }
   
   function playCoffeeShopAudio() {
+    const checkActiveSound = btnCoffeeShop.classList.contains('active-sound')
+    if(!checkActiveSound) {
+      coffeeShopVolume.value = 50
+      coffeeShopAudio.volume = coffeeShopVolume.value / 100
+    }
+
     stopAudioPlaying()
     coffeeShopAudio.play()
     coffeeShopAudio.loop = true
@@ -39,6 +59,13 @@ export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
   }
   
   function playFireplaceAudio() {
+    const checkActiveSound = btnFireplace.classList.contains('active-sound')
+    if(!checkActiveSound) {
+      fireplaceVolume.value = 50
+      fireplaceAudio.volume = fireplaceVolume.value / 100
+    }
+
+
     stopAudioPlaying()
     fireplaceAudio.play()
     fireplaceAudio.loop = true
@@ -51,7 +78,7 @@ export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
   
   function stopAudioPlaying() {
     let audios = [forestAudio, rainAudio, coffeeShopAudio, fireplaceAudio]
-  
+
     for (const audio of audios) {
       if (audio.play) {
         audio.pause()
@@ -61,6 +88,7 @@ export function Sounds ({ btnForest, btnRain, btnCoffeeShop, btnFireplace }) {
         btnFireplace.classList.remove('active-sound')
       }
     }
+
   }
 
   function timeEnd() {
